bff3dbac7e393e3b5a53c724defae373a8c6fcbd
sulbin <tlsdnr1135@naver.com>
2022-09-10 23:52:57 +0900

    초기세팅

---
+++ src/main/java/com/sulbin/junittest/JUnitTestPracApplication.java
@@ -0,0 +1,13 @@
+package com.sulbin.junittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitTestPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnitTestPracApplication.class, args);
+    }
+
+}
